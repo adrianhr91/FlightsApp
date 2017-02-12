99dfa4420b954593af159a5823b24a057e862923
Adrian Hristov <adrianhr91@gmail.com>
2017-02-12 00:53:44 +0000

    added rest of static content

--- client/src/components/searchoptions/SearchOptions.js
+++ client/src/components/searchoptions/SearchOptions.js
@@ -4,14 +4,25 @@ import './SearchOptions.css';
 class SearchOptions extends React.Component {
   render() {
     return (
-      <div className='search-options'>
-        <div>
-          <span className='place-code'>{ this.props.originPlaceCode }</span>
-          <img src='/images/arrow-white.png' /> 
-          <span className='place-code'>{ this.props.destinationPlaceCode }</span>
+      <div>
+        <div className='search-summary'>
+          <div>
+            <span className='place-code'>{ this.props.originPlaceCode }</span>
+            <img src='/images/arrow-white.png' /> 
+            <span className='place-code'>{ this.props.destinationPlaceCode }</span>
+          </div>
+          <div className='search-info'>
+            { this.props.travellersCount } travellers, { this.props.cabinType }
+          </div>
         </div>
-        <div className='search-info'>
-          { this.props.travellersCount } travellers, { this.props.cabinType }
+
+        <div className='search-options'>
+            <span className='option'>Filter</span>
+            <span className='option'>Sort</span>
+            <span className='alert'>
+              <img className='alert-img' src='/images/alert.png' />
+              Price Alert
+            </span>
         </div>
       </div>
     );
